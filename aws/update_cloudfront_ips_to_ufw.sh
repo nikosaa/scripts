a964e852333e121b35ae1f5b12f9cf0b1fce3d75
Niko <internet@jousipallo.com>
2022-02-26 08:30:39 +0200

    First version

---
+++ aws/update_cloudfront_ips_to_ufw.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+old=`tempfile`
+new=`tempfile`
+echo Reading old IPs to ${old}
+ufw status numbered | awk -F'[[:space:]][[:space:]]+'  '/Amazon CloudFront/{print $3}' | sort > ${old}
+echo Reading new IPs to ${new}
+curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '[.prefixes[] | select(.service=="CLOUDFRONT").ip_prefix] | .[]'  | sort > ${new}
+
+echo Comparing results
+diff ${old} ${new} | while read i j ; do 
+  case "${i}" in
+   ">" )
+    echo ${j} to be added 
+    ufw allow from "$j" to any app "Apache Secure" comment "Amazon CloudFront";
+    ;;
+   "<" )
+    echo ${j} to be removed
+    ufw delete allow from "$i" to any app "Apache Secure"
+    ;;
+  esac
+done
+
+echo Removing temporary files
+rm ${old} ${new}
